fa5ffc8dbf7f13dc6fb744b90ba68c4617dca2c6
wjmn <jamin.wu07@gmail.com>
2023-08-20 16:25:51 -0400

    Updates

---
+++ precache-manifest.ee1f5d416c4731d7bcf89cfbc77bba6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10505381a2b76906d73f35df35992d1b",
+    "url": "/slipbox/favicon.ico"
+  },
+  {
+    "revision": "47c243f4b4a8aec4f9b302398698d6b7",
+    "url": "/slipbox/favicon.svg"
+  },
+  {
+    "revision": "34c492ae48c67bdd400eb3a17cad9ca0",
+    "url": "/slipbox/index.html"
+  },
+  {
+    "revision": "88c0e3c8455970aa0da940187c3cfc20",
+    "url": "/slipbox/manifest.json"
+  },
+  {
+    "revision": "710bb5b864a4dc2cbe4f",
+    "url": "/slipbox/static/css/main.0193de81.chunk.css"
+  },
+  {
+    "revision": "710bb5b864a4dc2cbe4f",
+    "url": "/slipbox/static/js/main.710bb5b8.chunk.js"
+  },
+  {
+    "revision": "21cee92e5f9b6692d736",
+    "url": "/slipbox/static/js/runtime~main.21cee92e.js"
+  },
+  {
+    "revision": "b17b319b1ad2b352f0ab",
+    "url": "/slipbox/static/js/vendors~main.b17b319b.chunk.js"
+  }
+]);
